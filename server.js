ba2acf9707d25d0ccd0ee7e8c115bfa26a0d34ed
shashank <shashankreact1729@gmail.com>
2024-12-24 18:41:29 +0530

    cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-// server.js
 const express = require("express");
+const cors = require("cors"); // Import cors module
 const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+// Use CORS middleware to allow cross-origin requests
+app.use(cors()); // This will allow all domains
 
 app.use("/api", userRoutes);
 
